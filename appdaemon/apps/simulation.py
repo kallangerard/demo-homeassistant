470abe84e3984e574b998005ed88b40c5173d835
kallangerard <kallan.gerard@gmail.com>
2020-07-30 13:58:01 +0800

    Created mock thermostat

--- appdaemon/apps/simulation.py
+++ appdaemon/apps/simulation.py
@@ -1,7 +1,6 @@
 import time
 
 import appdaemon.plugins.hass.hassapi as hass
-from thermostat import Thermostat
 from solar_diverter import Loads
 from mock import dryer
 from mock import pv
@@ -14,11 +13,17 @@ WASHING_MACHINE = washing_machine.WASHING_MACHINE
 
 class Simulation(hass.Hass):
     def initialize(self):
-        self.log("Running Simulation")
-        self.loads = Loads(self, self.args["heaters"])
+
+        self.thermostats = self._get_loads(self.args["loads"].items())
+        self.loads = Loads(self, self.thermostats)
         self.set_state("input_number.tick", state=0)
-        self.set_state("input_number.tick", state=0)
-        self.run_every(self.hourly, "now", 1)
+        # self._run_thermostat()
+        # self.run_every(self._run_thermostat, "now", 5)
+        self.run_every(self._run_thermostat, "now", 2)
+
+    def _get_loads(self, loads: dict):
+        thermostats = list(map(lambda x: x[1].get("thermostat"), loads))
+        return thermostats
 
     def hourly(self, *args):
         tick = int(float(self.get_state("input_number.tick")))
@@ -35,6 +40,7 @@ class Simulation(hass.Hass):
             state_string=WASHING_MACHINE[tick],
             tick=tick,
         )
+        self._run_thermostat()
         self.loads.control_loads()
 
     def _switch_load(self, entity_id: str, state_string: str, tick: int):
@@ -43,3 +49,39 @@ class Simulation(hass.Hass):
             self.turn_on(entity_id)
         elif state_string == "off":
             self.turn_off(entity_id)
+
+    def _run_thermostat(self, *args):
+        current_temperatures = self.get_state("input_number")
+        input_booleans = self.get_state("input_boolean")
+        heater_states = {k: v.get("state") for (k, v) in input_booleans.items()}
+
+        for load in self.args["loads"].items():
+            heater = load[1]["heater"]
+            if heater_states[heater] == "off":
+                heat_gain = 0.0
+            else:
+                heat_gain = float(load[1]["heat_gain"])
+
+            heat_loss = float(load[1]["heat_loss"])
+            thermal_mass = float(load[1]["thermal_mass"])
+            target_temperature = load[1]["temperature"]
+            current_temp = float(current_temperatures[target_temperature]["state"])
+            new_temp = (
+                current_temp + (heat_gain * thermal_mass) - (heat_loss * thermal_mass)
+            )
+            self.log(f"Setting {target_temperature} to {new_temp}")
+            self.set_value(target_temperature, new_temp)
+
+
+class Thermostat:
+    def __init__(self, app, thermostat):
+        self.app = app
+        self._heat_gain = thermostat.get("heat_gain")
+        self._heat_loss = thermostat.get("heat_loss")
+        self._heater = thermostat.get("heater")
+
+    def set_target_temperature(self, target_temperature: float):
+        pass
+
+    def get_target_temperature(self) -> float:
+        return self.app.get_state(self._heater, attribute="target_temperature")
--- appdaemon/apps/thermostat.py
+++ appdaemon/apps/thermostat.py
@@ -1,24 +1,4 @@
 import appdaemon.plugins.hass.hassapi as hass
 
 
-class App(hass.Hass):
-    def initialize(self):
-        for args in self.args["thermostats"]:
-            thermostat = self.args["thermostats"][args]
-            self.log(thermostat.get("heat_gain"))
-            self.log(thermostat.get("heat_loss"))
-            self.log(thermostat.get("heater"))
 
-
-class Thermostat:
-    def __init__(self, app, thermostat):
-        self.app = app
-        self._heat_gain = thermostat.get("heat_gain")
-        self._heat_loss = thermostat.get("heat_loss")
-        self._heater = thermostat.get("heater")
-
-    def set_target_temperature(self, target_temperature: float):
-        pass
-
-    def get_target_temperature(self) -> float:
-        return self.app.get_state(self._heater, attribute="target_temperature")
